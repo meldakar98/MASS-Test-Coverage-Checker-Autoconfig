275d426b9fe97da53980f6be2342ead04f0c19e0
uwameni <uwameni@sw.buhl-data.com>
2023-08-05 19:29:34 +0200

    commit function getStringCoverageConfig ready

--- src/ts/Controller/MASS_CheckerCoverage.ts
+++ src/ts/Controller/MASS_CheckerCoverage.ts
@@ -9,13 +9,28 @@ export default class MASS_CheckerCoverage{
 
     constructor(showTestFailures?:boolean, showFullCoverageReport?:boolean, feedback?:CheckerCoverageFeedback[]){
         this.showTestFailures = showTestFailures !== undefined ? showTestFailures : true;
-        this.showFullCoverageReport = showTestFailures !== undefined ? showTestFailures : false;
+        this.showFullCoverageReport = showFullCoverageReport !== undefined ? showFullCoverageReport : false;
         this.feedback = feedback !== undefined ? feedback : [];
     }
 
     public getStringCoverageConfig() : string{
-        //TODO
-        return "";
+        let sFCR = this.showFullCoverageReport ? "true" : "false";
+        let sTF = this.showTestFailures ? "true" : "false";
+        let coverageString = '  "coverage": {\n    '+
+                            '"feedback": [\n      '+
+                            this.getStringFeedbackConfig() +
+                            '\n    ],\n    '+
+                            '"showFullCoverageReport": '+ sFCR +',\n    '+
+                            '"showTestFailures": '+ sTF +'\n  },\n';
+        return coverageString;
     }
 
+    public getStringFeedbackConfig() : string {
+        var buildedFeedback = this.feedback.length > 0 ? "" : new CheckerCoverageFeedback().buildPartFeedbackBlock_empty();
+        for(let i=0; i<this.feedback.length; i++){
+            buildedFeedback += this.feedback[i].buildPartFeedbackBlock();
+            buildedFeedback += i+1 == this.feedback.length ? "" : ",\n       ";
+        }
+        return buildedFeedback;
+    }
 }
