94f05c425b6f1ff08fc9d62c4b0c0453a65aa6ab
uwameni <uwameni@sw.buhl-data.com>
2023-08-22 15:32:55 +0200

    Commit function to download external .zip

--- src/ts/Controller/Helper.ts
+++ src/ts/Controller/Helper.ts
@@ -1,4 +1,3 @@
-import { time } from "console";
 
 export function readFile(file:File): Promise<string> {
     return new Promise((resolve, reject) => {
@@ -52,4 +51,58 @@ export function calculateTxtFileWeight(text: string, round): number {
   const totalKilobytes = totalBytes / 1024;
     
   return Math.round(totalKilobytes * (10**round)) / 100;
+}
+
+
+
+/**
+ * Download a zip file from a given URL and put it in an input file field.
+ * 
+ * @param url The URL of the zip file to download.
+ * @param inputFieldId The ID of the input file field to put the downloaded file in.
+ * @returns Returns a Promise that resolves when the file is successfully downloaded and put in the input field, or rejects with an error message.
+ */
+export function downloadZipFile(url: string, inputFieldId: string): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+      // Validate the input
+      if (!url || !inputFieldId) {
+          reject("Invalid input. URL and input field ID are required.");
+      }
+
+      // Fetch the zip file
+      fetch(url)
+          .then(response => {
+              if (!response.ok) {
+                  throw new Error(`Failed to download zip file. Status: ${response.status} ${response.statusText}`);
+              }
+              return response.blob();
+          })
+          .then(blob => {
+              // Create a new FileReader
+              const reader = new FileReader();
+
+              // Set the onload event handler to put the file contents in the input field
+              reader.onload = () => {
+                  const fileContents = reader.result;
+                  console.log(fileContents);
+                  const inputFile = document.getElementById(inputFieldId) as HTMLInputElement;
+                  const fileValue = new File([fileContents], "downloaded.zip");
+
+                  // Create a DataTransfer to get a FileList
+                  const dataTransfer = new DataTransfer();
+                  dataTransfer.items.add(fileValue);
+
+                  //Add files in principal file's input-field
+                  inputFile.files = dataTransfer.files;
+                  resolve();
+              };
+
+              // Read the blob as Array Buffer
+              reader.readAsArrayBuffer(blob);
+            
+          })
+          .catch(error => {
+              reject(`Failed to download zip file. ${error.message}`);
+          });
+  });
 }
