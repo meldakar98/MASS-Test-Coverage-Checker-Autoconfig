c97db00fe6e0f0cc741653372a23e662d71345c7
uwameni <uwameni@sw.buhl-data.com>
2023-08-24 20:30:14 +0200

    Function to structure a text in json form

--- src/ts/Controller/MASSHandler.ts
+++ src/ts/Controller/MASSHandler.ts
@@ -15,6 +15,7 @@ export default class MASSHandler{
     classSelected: boolean = false;
     metricsSelected: boolean = false;
 
+
     constructor(styleSelected?: boolean, semanticSelected?: boolean, coverageSelected?: boolean, classSelected?: boolean, metricsSelected?: boolean){
         this.styleSelected = styleSelected !== undefined ? styleSelected : false;
         this.semanticSelected = semanticSelected !== undefined ? semanticSelected : false;
@@ -22,6 +23,7 @@ export default class MASSHandler{
         this.classSelected = classSelected !== undefined ? classSelected : false;
         this.metricsSelected = metricsSelected !== undefined ? metricsSelected : false;
     }
+
 
     public getDefault_massFullConfig(): string{
         let syntax : MASS_Syntax = new MASS_Syntax(); 
@@ -42,12 +44,75 @@ export default class MASSHandler{
             '}';
     }
 
+
     public getDefault_massSelected(): string{
         return '\n  "styleSelected": '+this.styleSelected.toString()+',\n  "semanticSelected": '+this.semanticSelected.toString()+',\n  "coverageSelected": '+this.coverageSelected.toString()+',\n  "classSelected": '+this.classSelected.toString()+',\n  "metricsSelected": '+this.metricsSelected.toString()+',';
     }
+
 
     public isCorrectConfigSkeleton(configTxt: string): boolean{
         return isCorrectJsonSkeleton(configTxt, this.getDefault_massFullConfig());
     }
 
-} 
+
+    public formatConfigResult(configTxt: string, numberSpaces: number, isSpaceStart:boolean = false): string{
+        //default space between key and container rand
+        let space_default = '  ';
+
+        //variable space between key and container rand depending on numberSpaces : number of childs in object's value
+        let space = '  ';
+        for(let i=0; i<numberSpaces; i++){
+            space += space;
+        }
+
+        //Cast @param configTxt in json object
+        let jsonConfig = JSON.parse(configTxt);
+        const keysConfig = Object.keys(jsonConfig);
+
+        //Add opening brace, which represent the start of the object
+        var result =  isSpaceStart ? space.substring(space_default.length) + '{' : '{';
+        
+        //index of current key in [keysConfig] array of found keys
+        let indexParam = 1;
+
+        //Iterate through array of keys in constructed json object from given @param configTxt
+        for (let key of keysConfig) {
+            let commaParam = indexParam < keysConfig.length ? ',' : '';
+            result += '\n'+ space + '"' + key + '": ';
+            const currObjValue = jsonConfig[key];
+
+            //if the value from the key is an array
+            if (Array.isArray(currObjValue)) {
+                result += '[\n';
+                if(typeof currObjValue[0] == "object"){
+                    let indexParamArr = 1;
+                    for (let arrKey in currObjValue) {
+                        commaParam = indexParamArr < currObjValue.length ? ',\n' : '';
+                        result += this.formatConfigResult(JSON.stringify(currObjValue[arrKey]), numberSpaces+1, true) + commaParam;
+                        indexParamArr++;
+                    }
+                }
+                result += '\n' + space + '],';
+            } 
+            //if the value from the key is an object
+            else if (typeof currObjValue === "object") {
+                result += this.formatConfigResult(JSON.stringify(currObjValue), numberSpaces+1) + commaParam;
+            } 
+            // if the value from the key is a string
+            else if (typeof currObjValue === "string"){
+                result += '"'+ currObjValue +'"' + commaParam;
+            }
+            //anything else : numbers, enums etc...
+            else {
+                result += '"'+ currObjValue.toString() +'"' + commaParam ;
+            }
+            indexParam++;
+        }
+
+        //Add closing brace, which represent the end of the object
+        return result + (keysConfig.length > 0 ? '\n' + space.substring(space_default.length) + '}': '}');
+    }
+
+
+
+}
