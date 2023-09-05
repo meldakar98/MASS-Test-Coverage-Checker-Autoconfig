c0e44c3e379bc5f4d16b7746361790f581a652ed
uwameni <uwameni@sw.buhl-data.com>
2023-09-06 01:47:25 +0200

    Remove Controlling Function from Model MASS_Syntax

--- src/ts/Model/MASS_Syntax.ts
+++ src/ts/Model/MASS_Syntax.ts
@@ -1,5 +1,4 @@
 import {Level} from "./DataStructurs/Level.js";
-import MASSHandler from "../Controller/MASSHandler.js";
 
 export default class MASS_Syntax{
     level: Level = Level.BEGINNER;
@@ -20,10 +19,4 @@ export default class MASS_Syntax{
         return '\n  "syntax": {\n    "level": "BEGINNER"\n  },';
     }
 
-    public updateResult(){
-        let resultContainer = document.querySelector(".overview_result textarea.boxContainer") as HTMLTextAreaElement;
-        let resultJson = JSON.parse(resultContainer.value);
-        resultJson["syntax"]["level"] = Level[this.level];
-        resultContainer.value = new MASSHandler().formatConfigResult(JSON.stringify(resultJson), 1);
-    }
 }
