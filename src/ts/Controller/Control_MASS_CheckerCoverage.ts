10d846a38896d9b61e11179fc9fcc2afc748c970
uwameni <uwameni@sw.buhl-data.com>
2023-09-06 02:03:05 +0200

    Add Controller Control_MASS_CheckerCoverage

--- src/ts/Controller/Control_MASS_CheckerCoverage.ts
+++ src/ts/Controller/Control_MASS_CheckerCoverage.ts
@@ -1,6 +1,6 @@
 import MASSHandler from "./MASSHandler.js";
-import { CoverageMiss } from "../Model/DataStructurs/CoverageMiss.js";
 import CheckerCoverageFeedback from "../Model/CheckerCoverageFeedback.js";
+import MASS_CheckerCoverage from "../Model/MASS_CheckerCoverage.js";
 import LineRanges from "../Model/DataStructurs/LineRanges.js";
 import { readFile } from "./Helper.js";
 
@@ -8,37 +8,13 @@ interface AssociativeArrayFeedbacks {
   [key: string]: CheckerCoverageFeedback
 }
 
-export default class MASS_CheckerCoverage {
-  showTestFailures: boolean = true;
-  showFullCoverageReport: boolean = false;
-  feedback: AssociativeArrayFeedbacks[] = [];
+export default class Control_MASS_CheckerCoverage extends MASS_CheckerCoverage{
 
   constructor(showTestFailures?: boolean, showFullCoverageReport?: boolean, feedback?: AssociativeArrayFeedbacks[]) {
-    this.showTestFailures = showTestFailures !== undefined ? showTestFailures : true;
-    this.showFullCoverageReport = showFullCoverageReport !== undefined ? showFullCoverageReport : false;
-    this.feedback = feedback !== undefined ? feedback : [];
-  }
-
-  public getStringCoverageConfig(): string {
-    let sFCR = this.showFullCoverageReport ? "true" : "false";
-    let sTF = this.showTestFailures ? "true" : "false";
-    let coverageString = '\n  "coverage": {\n    ' +
-      '"feedback": [\n      ' +
-      this.getStringFeedbackConfig() +
-      '\n    ],\n    ' +
-      '"showFullCoverageReport": ' + sFCR + ',\n    ' +
-      '"showTestFailures": ' + sTF + '\n  },';
-    return coverageString;
-  }
-
-  public getStringFeedbackConfig(): string {
-    let fbKeys = Object.keys(this.feedback);
-    var buildedFeedback = fbKeys.length > 0 ? "" : new CheckerCoverageFeedback().buildPartFeedbackBlock_empty();
-    for (let fbKey in this.feedback) {
-      buildedFeedback += this.feedback[fbKey as string].buildPartFeedbackBlock();
-      buildedFeedback += fbKey == fbKeys[fbKeys.length - 1] ? "" : ",\n    ";
-    }
-    return buildedFeedback;
+    let showTestFailuresParent = showTestFailures !== undefined ? showTestFailures : true;
+    let showFullCoverageReportParent = showFullCoverageReport !== undefined ? showFullCoverageReport : false;
+    let feedbackParent = feedback !== undefined ? feedback : [];
+    super(showTestFailuresParent, showFullCoverageReportParent, feedbackParent);
   }
 
 
@@ -99,7 +75,6 @@ export default class MASS_CheckerCoverage {
             }
           }
         } else if (line.includes('/*')) {
-          //TODO handle muti-lines
           //find start and end of comment
           let endCommentLineNumber: number = lineNumber;
           let comment: string = "";
@@ -188,7 +163,7 @@ export default class MASS_CheckerCoverage {
   //1- Build array of syntactic errors & build array of feedback block
   //2- Parse coverage value of config and replace completely with new results
   public async buildConfigFromJavaFiles(files, isToUnpack:boolean, isReplacingOld: boolean) {
-    var cc : MASS_CheckerCoverage = new MASS_CheckerCoverage();
+    var cc : Control_MASS_CheckerCoverage = new Control_MASS_CheckerCoverage();
     var isStillReplacingOld = isReplacingOld;
     var relativePaths = [];
     var currentFiles = [];
@@ -271,47 +246,6 @@ export default class MASS_CheckerCoverage {
     
     resultTxt["coverageSelected"] = true;
     resultContainer.value = new MASSHandler().formatConfigResult(JSON.stringify(resultTxt), 1);
-  }
-
-
-  private extractFeedbackPart(lines, line, commentStartString, kwFirstCharLineNumber, kwLastCharLineNumber): any[] {
-    const keyWordStartCov: string = '@mass_cvStart(';
-    //fetch parameters
-    let startParams = line.substring(line.indexOf(keyWordStartCov));
-    startParams = startParams.substring(startParams.indexOf('"') + 1);
-    let params = startParams.substring(0, startParams.lastIndexOf('"'));
-    let paramsConcat = params;
-    let currPartConcatParam = "";
-    let arrayParams = [];
-    while (paramsConcat.indexOf('",') >= 0) {
-      currPartConcatParam += paramsConcat.substring(0, paramsConcat.indexOf('",'));
-      if ((paramsConcat.indexOf('\",') != -1 && (paramsConcat.indexOf('",') == paramsConcat.indexOf('\",') + 1)) || (paramsConcat.indexOf('\\",') != -1 && (paramsConcat.indexOf('",') == paramsConcat.indexOf('\\",') + 2))) {
-        paramsConcat = paramsConcat.substring(paramsConcat.indexOf('",') + 2);
-      } else {
-        arrayParams.push(currPartConcatParam);
-        paramsConcat = paramsConcat.substring(paramsConcat.indexOf('",') + 2);
-        paramsConcat = paramsConcat.substring(paramsConcat.indexOf('"') + 1);
-        currPartConcatParam = "";
-      }
-    }
-    arrayParams.push(paramsConcat);
-    //TODO if array bigger than 4 elements, save errors. else
-    //id : arrayParams[0]
-    arrayParams[0] = arrayParams[0].replaceAll(/\s/g, '');
-    //message : arrayParams[1];
-    //coverageMiss : arrayParams[2] 
-    arrayParams[2] = arrayParams[2].replaceAll(/\s/g, '') == CoverageMiss.FULLY_MISSED.toString() ? CoverageMiss.FULLY_MISSED : CoverageMiss.PARTIALLY_MISSED;
-    //supMessages : arrayParams[3]
-    arrayParams[3] = arrayParams[3].replaceAll(/\s/g, '').split(',');
-    // Find the next non-empty Java instruction line
-    let nextInstructionLine: number = line.trim().startsWith(commentStartString) ? kwLastCharLineNumber + 2 : kwLastCharLineNumber + 1;
-    while (nextInstructionLine != kwLastCharLineNumber && nextInstructionLine < lines.length) {
-      if (lines[nextInstructionLine].trim() != '')
-        break;
-      nextInstructionLine++;
-    }
-    arrayParams.push(nextInstructionLine);
-    return arrayParams;
   }
 
 }
